6ce11554d9bc23b7da6575a7b1fdd7c871fda9ab
Biraj1122 <sharmabiraz@gmail.com>
2026-05-09 21:22:08 +0545

    Splash Screen and bugs Fix

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -214,7 +214,7 @@ class HomeScreen extends StatelessWidget {
                   final products = [
                     ["assets/images/tomatoes.png", "Fresh Tomatoes", "120", "1kg"],
                     ["assets/images/potato png.png", "Organic Potatoes", "80", "1kg"],
-                    ["assets/images/green cabbage.png", "Green Cabbage", "60", "1pc"],
+                    ["assets/images/green cabbage.png", "Green Cabbage", "60", "1kg"],
                     ["assets/images/milk png.png", "Farm Fresh Milk", "110", "1L"],
                   ];
 
