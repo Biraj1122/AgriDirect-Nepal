5ecd53abbb79092f959d6853faf6676060608df5
yathu258 <anianyone235@gmail.com>
2026-05-22 08:03:35 +0545

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:farmtech_agridirect/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
